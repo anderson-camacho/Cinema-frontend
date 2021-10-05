65fd7bcfd45d17d92d93f9bf5eae68f2b7d79f82
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-05 15:49:25 -0500

    se hce la modificacion del codigopra remplsar lo que se tenia en listar en la funcionalidad real que es eliminar en el componente eliminar

--- src/app/feature/pelicula/components/borrar-pelicula/borrar-pelicula.component.ts
+++ src/app/feature/pelicula/components/borrar-pelicula/borrar-pelicula.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pelicula } from '@pelicula/shared/model/pelicula';
 import { PeliculaService } from '@pelicula/shared/service/pelicula.service';
+import { delay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-borrar-pelicula',
@@ -10,20 +12,26 @@ import { PeliculaService } from '@pelicula/shared/service/pelicula.service';
 })
 
 export class BorrarPeliculaComponent implements OnInit {
+  peliculaId: number;
   peliculaForm: FormGroup;
   pelicula = {} as Pelicula;
-  constructor(protected peliculaService: PeliculaService) { }
+  constructor(protected peliculaService: PeliculaService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-
+    this.peliculaId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+    this.onSubmitDelete(this.peliculaId);
   }
 
-  onSubmitDelete(pelicula: Pelicula): void {
-    this.peliculaService.eliminarPelicula(pelicula).subscribe(
+  onSubmitDelete(valueId: number ): void {
+    this.peliculaService.eliminarPelicula(valueId)
+    .pipe(
+      tap(()=> this.router.navigate(['listar_pelicula'])),
+      delay(2000)
+    )
+    .subscribe(
       data => {
         console.log(data);
         this.ngOnInit();
-
       },
       error => { console.log(error) }
     );
