a19c1b6c3797538d1e52941eddacd5287ccfc4f8
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-06 17:17:46 -0500

    se agrega el routing de usuario el listar

--- src/app/feature/usuario/usuario-routing.module.ts
+++ src/app/feature/usuario/usuario-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarUsuarioComponent } from './components/listar-usuario/listar-usuario.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 const routes: Routes = [
   {
     path: '',
     component: UsuarioComponent,
-    children: []
+    children: [
+      {
+        path: 'listar',
+        component: ListarUsuarioComponent
+      }
+    ]
   }
 ];
 
