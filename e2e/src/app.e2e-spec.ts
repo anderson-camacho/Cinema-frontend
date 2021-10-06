d40d53849e8c90c2f3c0877fab81fa3c058f632a
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-06 07:55:28 -0500

    Se cambia titulos en la prueba e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Aplicacion Cinema', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Angular Seed');
+    expect(page.getTitleText()).toEqual('CINEMA');
   });
 
   afterEach(async () => {
