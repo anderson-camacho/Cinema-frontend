9b3c71997263f048ccaa9d0324001ac0dee6eed7
Anderson Camacho <anderson_camacho@outlook.com>
2021-10-08 09:43:34 -0500

    Eliminar stylos no usados de peliculas

--- src/app/feature/pelicula/components/pelicula/pelicula.component.ts
+++ src/app/feature/pelicula/components/pelicula/pelicula.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pelicula',
-  templateUrl: './pelicula.component.html',
-  styleUrls: ['./pelicula.component.scss']
+  templateUrl: './pelicula.component.html'
 })
 export class PeliculaComponent implements OnInit {
   constructor() { }
